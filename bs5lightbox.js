d9ab9709c0ec765f8ecc652036890d918a42571d
avalon-studio <91736125+avalon-studio@users.noreply.github.com>
2021-12-26 17:11:46 +0900

    Update bs5lightbox.js

--- bs5lightbox.js
+++ bs5lightbox.js
@@ -1,7 +1,7 @@
 const body_element = document.querySelector('html body');
 const modal_div = document.createElement("div");
 let modal_window = document.body.appendChild(modal_div);
-let modal_html = '<div class="modal fade" id="lightboxModalFullscreen" tabindex="-1" aria-labelledby="lightboxModalFullscreenLabel" aria-hidden="true"><div class="modal-dialog modal-fullscreen" data-bs-dismiss="modal" aria-label="Close"><div class="modal-content py-2 border-0"><div class="modal-body d-flex align-self-center align-items-center justify-content-center"><img src="" class="img-fluid" id="LightboxCanvas" data-bs-dismiss="modal" aria-label="Close" /></div></div></div></div>'
+let modal_html = '<div class="modal fade" id="lightboxModalFullscreen" tabindex="-1" aria-labelledby="lightboxModalFullscreenLabel" aria-hidden="true"><div class="modal-dialog modal-fullscreen" data-bs-dismiss="modal" aria-label="Close"><div class="modal-content border-0"><div class="modal-body d-flex align-self-center align-items-center justify-content-center"><img src="" id="LightboxCanvas" data-bs-dismiss="modal" aria-label="Close" /></div></div></div></div>'
 modal_window.innerHTML = modal_html;
 
 Array.from(document.querySelectorAll('[data-modal*="bs-lightbox"]')).forEach((href) => {
